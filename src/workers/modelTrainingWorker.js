cbc1164ce66b496655427585fcad2958db17ac8c
Matheus Braga Ribeiro <mbribeiropr@gmail.com>
2026-04-27 23:39:46 -0300

    feat: Adiciona conversão de catálogo para vetores

--- src/workers/modelTrainingWorker.js
+++ src/workers/modelTrainingWorker.js
@@ -1,12 +1,19 @@
 import 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js';
 import { workerEvents } from '../events/constants.js';
 
+const WEIGHTS = {
+    category: 0.4,
+    color: 0.3,
+    price: 0.2,
+    age: 0.1,
+};
+
 console.log('Model training worker initialized');
 let _globalCtx = {};
 
 const normalize = (value, min, max) => (value - min) / ((max - min) || 1);
 
-async function makeContext(users, catalog) {
+function makeContext(users, catalog) {
     const ages = users.map((u) => u.age);
     const prices = catalog.map((p) => p.price);
 
@@ -19,8 +26,8 @@ async function makeContext(users, catalog) {
     const categories = [...new Set(catalog.map((p) => p.category))];
     const colors = [...new Set(catalog.map((p) => p.color))];
     
-    const categoryIndex = Object.entries(categories.map((category, index) => [category, index]));
-    const colorIndex = Object.entries(colors.map((color, index) => [color, index]));
+    const categoryIndex = Object.fromEntries(categories.map((category, index) => [category, index]));
+    const colorIndex = Object.fromEntries(colors.map((color, index) => [color, index]));
     
     // Computar a média de idade dos compradores por produto
     // (ajuda a personalizar)
@@ -56,8 +63,39 @@ async function makeContext(users, catalog) {
         numCategories: categories.length,
         numColors: colors.length,
         // 1 (age) + 1 (price) + colors.length + categories.length
-        dimensions: 1 + 1 + colors.length + categories.length
+        dimensions: 1 + 1 + colors.length + categories.length,
+        productAvgAgeNorm,
     };
+}
+
+const oneHotWeighted = (index, length, weight) => tf.oneHot(index, length).cast("float32").mul(weight);
+
+function encodeProduct(product, context) {
+    const price = tf.tensor1d([normalize(
+        product.price,
+        context.minPrice,
+        context.maxPrice
+    ) * WEIGHTS.price]);
+
+    const age = tf.tensor1d([normalize(
+        context.productAvgAgeNorm[product.name],
+        context.minAge,
+        context.maxAge
+    ) * WEIGHTS.age]);
+
+    const category = oneHotWeighted(
+        context.categoryIndex[product.category],
+        context.numCategories,
+        WEIGHTS.category
+    );
+
+    const color = oneHotWeighted(
+        context.colorIndex[product.color],
+        context.numCategories,
+        WEIGHTS.color
+    );
+
+    return tf.concat1d([price, age, category, color]);
 }
 
 async function trainModel({ users }) {
@@ -67,9 +105,15 @@ async function trainModel({ users }) {
 
     const catalog = await (await fetch("/data/products.json")).json();
     
-    const context = await makeContext(users, catalog);
+    const context = makeContext(users, catalog);
 
-    debugger
+    context.productVectors = catalog.map((p) => ({
+        product: p.name,
+        meta: {...p},
+        vector: encodeProduct(p, context).dataSync(),
+    }));
+
+    _globalCtx = context;
 
     postMessage({
         type: workerEvents.trainingLog,
